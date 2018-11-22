09702faabb10efa59ffd53c9e74b7995b4482cb4
Ivan Grbavac <ivan.grbavac@live.com>
2018-11-22 22:29:34 +0100

    Added values

--- src/main/scala/RocksCacheApp.scala
+++ src/main/scala/RocksCacheApp.scala
@@ -1,3 +1,6 @@
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
+
 import io.finch._
 import cats.effect.IO
 import com.twitter.finagle.http.{Request, Response}
@@ -6,15 +9,17 @@ import com.twitter.util.Await
 import io.circe.generic.auto._
 import io.finch._
 import io.finch.circe._
-
 import org.rocksdb.RocksDB
 import org.rocksdb.Options
 
-case class Key(key: String)
+case class KeyValue(key: String, value: String)
 
 object RocksCacheApp extends App with Endpoint.Module[IO] {
 
   RocksDB.loadLibrary()
+
+  private def getBytesUTF8(value: String): Array[Byte] = StandardCharsets.UTF_8.encode(value).array
+  private def getStringUTF8(value: Array[Byte]) = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(value)).toString
 
   val options = new Options().setCreateIfMissing(true)
   val db = RocksDB.open(options, "/Users/igrbavac/Documents/Development/scala/rocks-cache/temp/rocksdb")
@@ -23,22 +28,22 @@ object RocksCacheApp extends App with Endpoint.Module[IO] {
     Ok("Hello, World!")
   }
 
-  final val postKey: Endpoint[IO, Key] = post("keys" :: jsonBody[Key]) { t: Key =>
-    db.put(t.key.getBytes, Array[Byte]())
+  final val postKey: Endpoint[IO, KeyValue] = post("keys" :: jsonBody[KeyValue]) { t: KeyValue =>
+    db.put(getBytesUTF8(t.key), getBytesUTF8(t.value))
     println(t)
     Ok(t)
   }
 
-  final val getKey: Endpoint[IO, Key] = get("keys" :: path[String]) { key: String =>
-    val value = db.get(key.getBytes)
+  final val getValue: Endpoint[IO, String] = get("keys" :: path[String]) { key: String =>
+    val value = db.get(getBytesUTF8(key))
     value match {
-      case null => NotFound(new Exception("Key not fount"))
-      case _ => Ok(Key(key))
+      case null => NotFound(new Exception("Key not found"))
+      case _ => Ok(getStringUTF8(value))
     }
   }
 
   val service: Service[Request, Response] = Bootstrap
-    .serve[Application.Json](sayHello :+: postKey :+: getKey).toService
+    .serve[Application.Json](sayHello :+: postKey :+: getValue).toService
 
   Await.ready(Http.server.serve(":8080", service))
 }
