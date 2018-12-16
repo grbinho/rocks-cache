c43a51f2d1a8b7cb30a50b5e84a9fb9e8bdad11c
Ivan Grbavac <ivan.grbavac@live.com>
2018-12-16 17:59:14 +0100

    Started transforming statistics to http endpoint json

--- src/main/scala/org/rockscache/RestService.scala
+++ src/main/scala/org/rockscache/RestService.scala
@@ -8,10 +8,21 @@ import io.finch._
 import io.finch.circe._
 import org.rocksdb.HistogramType
 
+case class HistogramData(median: Double, percentile95: Double, percentile99: Double, average: Double, standardDeviation: Double)
+object HistogramData {
+  def apply(data: org.rocksdb.HistogramData): HistogramData = {
+    HistogramData(data.getMedian, data.getPercentile95, data.getPercentile99, data.getAverage, data.getStandardDeviation)
+  }
+}
+
+case class DbStatistics(dbWrite: HistogramData, dbGet: HistogramData)
+
 class RestService(cacheDb: CacheStoreDatabase) extends Endpoint.Module[IO] {
-  final val getStats: Endpoint[IO, String] = get("stats") {
-    val dbGetHistogram = cacheDb.statisticsObject.getHistogramString(HistogramType.DB_GET)
-    Ok(dbGetHistogram)
+  final val getStats: Endpoint[IO, DbStatistics] = get("stats") {
+    val writes = HistogramData(cacheDb.statisticsObject.getHistogramData(HistogramType.DB_WRITE))
+    val reads = HistogramData(cacheDb.statisticsObject.getHistogramData(HistogramType.DB_GET))
+    val stats = DbStatistics(writes, reads)
+    Ok(stats)
   }
 
   final val httpService: Service[Request, Response] =
